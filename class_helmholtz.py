931fcd28cb069eb96c544b07611dec95393fe542
chanbuntu <chanchihin_1@hotmail.com>
2020-04-16 15:41:27 +0100

    Finalising plots

--- class_helmholtz.py
+++ class_helmholtz.py
@@ -8,6 +8,7 @@ Created on Wed Apr 15 09:14:07 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 from math import pi
 from math import cos
 
@@ -50,22 +51,32 @@ class Helmholtz:
         # Define Shape Functions
         
         
-    def gauss_lobatto_mass(self):
+    def init_gauss_lobatto(self):
         """
         Implements Numerical Integration using Gauss Lobatto
         Stores the value of phi at quadrature points -1, 0, 1
-        Used for construct Mass Matrix
+        i -> phi_i, j -> phi_i evaluated at quadrature point j
         """
         phi = np.zeros((self.p+1, 3))
+        dphi = np.zeros((self.p+1, 3))
         if (self.p == 1):
+            # phi values evaluated at quadrature points
             phi[0,0] = 1.0
             phi[0,1] = 0.5
             phi[0,2] = 0.0
             phi[1,0] = 0.0
             phi[1,1] = 0.5
             phi[1,2] = 1.0
+            # d(phi)/d(psi) values evaluted at quadrature points
+            dphi[0,0] = -0.5
+            dphi[0,1] = -0.5
+            dphi[0,2] = -0.5
+            dphi[1,0] = 0.5
+            dphi[1,1] = 0.5
+            dphi[1,2] = 0.5
             
         elif (self.p == 2):
+            # phi values evaluated at quadrature points
             phi[0,0] = 1.0
             phi[0,1] = 0.0
             phi[0,2] = 0.0
@@ -75,25 +86,7 @@ class Helmholtz:
             phi[2,0] = 0.0
             phi[2,1] = 0.0
             phi[2,2] = 1.0
-            
-        self.phi = phi
-     
-    def gauss_lobatto_laplacian(self):
-        """
-        Implements Gauss Lobatto Quadrature
-        Stores the value of dphi at quadrature points -1, 0, 1
-        Used to construct Laplacian Matrix
-        """
-        dphi = np.zeros((self.p+1, 3))
-        if (self.p == 1):
-            dphi[0,0] = -0.5
-            dphi[0,1] = -0.5
-            dphi[0,2] = -0.5
-            dphi[1,0] = 0.5
-            dphi[1,1] = 0.5
-            dphi[1,2] = 0.5
-            
-        elif (self.p == 2):
+            # d(phi)/d(psi) values evaluted at quadrature points
             dphi[0,0] = -3/2
             dphi[0,1] = -1/2
             dphi[0,2] = 1/2
@@ -104,16 +97,18 @@ class Helmholtz:
             dphi[2,1] = 1/2
             dphi[2,2] = 3/2
             
+        self.phi = phi
         self.dphi = dphi
+     
         
-    def MatElem(self):
+    def ConstructElem(self):
         """
         Constructing elemental matrices
         """
         
-        self.MElem = np.zeros((self.p+1,self.p+1))
-        self.LElem = np.zeros((self.p+1,self.p+1))
-        self.FElem = np.zeros((self.N_el,self.p+1))
+        MElem = np.zeros((self.p+1,self.p+1))
+        LElem = np.zeros((self.p+1,self.p+1))
+        FElem = np.zeros((self.N_el,self.p+1))
 
         # Assigning Quadrature Weights
         weights = np.zeros((3,1))
@@ -128,27 +123,23 @@ class Helmholtz:
         for i in range(self.p+1):
             for j in range(self.p+1):
                 for k in range(3):
-                    self.MElem[i,j] += self.phi[i,k] * self.phi[j,k] * self.dx/2.0 * weights[k]
-                    self.LElem[i,j] += self.dphi[i,k] * self.dphi[j,k] * self.dx/2.0 * (2.0/self.dx)**2 * weights[k]
+                    MElem[i,j] += self.phi[i,k] * self.phi[j,k] * self.dx/2.0 * weights[k]
+                    LElem[i,j] += self.dphi[i,k] * self.dphi[j,k] * self.dx/2.0 * (2.0/self.dx)**2 * weights[k]
         
         
         for e in range(self.N_el):
             for j in range(self.p+1):
                 for k in range(3):
-                    self.FElem[e,j] += self.phi[j,k] * weights[k] * self.dx / 2.0 * source(e*self.dx + j*self.dx/self.p)
+                    FElem[e,j] += self.phi[j,k] * weights[k] * self.dx / 2.0 * source(e*self.dx + j*self.dx/self.p)
         
-        # Checking
-#        print("Elemental F Vector")
-#        print(self.FElem)
-#        print("Elemental Mass Matrix:")
-#        print(self.MElem)
-#        print("Elemental Laplacian Matrix: ")
-#        print(self.LElem)
+        return [MElem, LElem, FElem] 
+
+    def ConstructGlob(self, MElem, LElem, FElem):
         
-    def MatGlob(self):
         MG = np.zeros((self.n_dof, self.n_dof))
         LG = np.zeros((self.n_dof, self.n_dof))
-        
+        FG = np.zeros(self.n_dof)
+
         # Defines connectivity map
         connect = np.zeros(self.p+1, dtype = int)
         
@@ -156,49 +147,43 @@ class Helmholtz:
         for i in range(self.p+1):
             connect[i] = i
             
+        # Constructing Global Mass/Laplacian Matrices    
         for e in range(self.N_el):
             for i in range(self.p+1):
                 for j in range(self.p+1):
-                    MG[connect[i], connect[j]] = MG[connect[i],connect[j]] + self.MElem[i,j]
-                    LG[connect[i], connect[j]] = LG[connect[i],connect[j]] + self.LElem[i,j]
+                    MG[connect[i], connect[j]] = MG[connect[i],connect[j]] + MElem[i,j]
+                    LG[connect[i], connect[j]] = LG[connect[i],connect[j]] + LElem[i,j]
             # Updating connectivity map
             connect = connect + self.p
-        
-        # Prints Global Matrices for Checking
-        self.MG = MG
-        self.LG = LG
-        
-    def SourceGlob(self):
-        
-        self.FGlob = np.zeros(self.n_dof)
-        
-        # Defines connectivity map
-        connect = np.zeros(self.p+1, dtype = int)
         
         # Intialising Connectivity map
         for i in range(self.p+1):
             connect[i] = i
-            
+        
+        # Constructing Global Source Vector
         for e in range(self.N_el):
             for i in range(self.p + 1):
-                self.FGlob[connect[i]] = self.FGlob[connect[i]] + self.FElem[e,i]
+                FG[connect[i]] = FG[connect[i]] + FElem[e,i]
             connect = connect + self.p
+        
+        return [MG, LG, -FG]
             
-        self.FGlob = -self.FGlob
-    
-    def Solve(self):
+    def Solve(self, MG, LG, FG):
         
         # Setting up Stiff Matrix and RHS for the homogeneous problem
-        stiff = self.LG + self.lmda*self.MG
-        stiff = stiff[1:-1,1:-1]
-        rhs = self.FGlob[1:-1]
+        stiff = LG + self.lmda*MG
+        rhs = FG
+        
+        # Using Method of Large Number
+        large = 10e15
+        stiff[0,0] = large
+        rhs[0] = large*self.alpha
         
         # Solving for the homogenous Solution
-        uH = np.zeros(self.n_dof)
-        uH = np.linalg.inv(stiff)@rhs
+        u = np.zeros(self.n_dof)
+        u = np.linalg.inv(stiff)@rhs
         
         # Solution by adding Dirchlet Condition
-        u = uH + self.alpha
         x = np.linspace(0,1,self.n_dof)
    
         # Computing Exact Solution
@@ -206,10 +191,130 @@ class Helmholtz:
         for i in range(len(x)):
             u_ex[i] = cos(2*pi*x[i])
         
+        # Computing the L2-norm
+        l2 = 0.0
+        for i in range(len(x)):
+            l2 += (u_ex[i] - u[i])**2
+        l2 = math.sqrt(l2/(len(x)))
+        print("L2 Norm: " + str(l2) + '\n')
         
-        plt.plot(x[1:-1], u)
-        plt.plot(x, u_ex)
+        self.l2 = l2
+        self.x = x
+        self.u = u
+        self.u_ex = u_ex
+    
+    def PlotOnce(self):
+        
+        plt.close()
+        if (self.p == 1):
+            title_text = ("Linear Elements, $N_{el} = $" 
+                          + str(self.N_el) 
+                          + '\n' + 'dx: ' + str(self.dx) 
+                          + '   L2-$norm$: ' +str("{:.3f}".format(self.l2)))
+        elif(self.p == 2):
+            title_text = ("Quadratic Elements, $N_{el} = $" 
+                          + str(self.N_el) + '\n' 
+                          + 'dx: ' + str(self.dx) 
+                          + '   L2-$norm$: ' +str("{:.3f}".format(self.l2)))
+            
+        plt.figure()
+        plt.plot(self.x, self.u, '-o', label = "FEM Solution")
+        plt.plot(self.x, self.u_ex, 'r--', linewidth = '1.6', label = "Exact Solution")
+        plt.title(title_text)
+        plt.xlabel('x')
+        plt.ylabel('u')
+        plt.legend()
         plt.grid()
+        
+
+    def ErrorAnalysis(self):
+        
+        # Declaring an array of elements to be solved
+        NN_el = np.array([5, 10, 20, 50, 100])
+        lin_err = np.zeros(len(NN_el))
+        quad_err = np.zeros(len(NN_el))
+        
+        plt.figure()
+        # Computing L2-norms for linear elements
+        for i in range(len(NN_el)):
+            linear = Helmholtz(NN_el[i],1)
+    
+            # Initialising phi/dphi values at Gauss-Lobatto quadrature points
+            linear.init_gauss_lobatto()
+            
+            # Building Elemental Mass, Laplacian and Source matrices/vectors
+            [MElem, LElem, FElem] = linear.ConstructElem()
+            
+            # Constructing Global Mass, Laplacian and Source matrices/vectors
+            [MG, LG, FG] = linear.ConstructGlob(MElem, LElem, FElem)
+            
+            # Assemble Stiffness matrix and solving Ax = b
+            # Dirichlet B.Cs are solved using "Method of Large Number"
+            linear.Solve(MG, LG, FG)
+            
+            lin_err[i] = linear.l2
+            
+            # Plotting Results for All linear elements
+            plt.plot(linear.x, linear.u, '-', linewidth = '1', label = 'Grid Size: ' + str(linear.dx) )
+        
+        # Adding exact solution plot
+        plt.plot(linear.x, linear.u_ex, 'r--', linewidth = '1.6', label = "Exact Solution")
+        plt.title('Linear Elements')
+        plt.xlabel('x')
+        plt.ylabel('u')
+        plt.legend()
+        plt.grid()
+        
+        
+        # Computing L2-norms for quadratic elements
+        plt.figure()
+        for i in range(len(NN_el)):
+            quad = Helmholtz(NN_el[i],2)
+    
+            # Initialising phi/dphi values at Gauss-Lobatto quadrature points
+            quad.init_gauss_lobatto()
+            
+            # Building Elemental Mass, Laplacian and Source matrices/vectors
+            [MElem, LElem, FElem] = quad.ConstructElem()
+            
+            # Constructing Global Mass, Laplacian and Source matrices/vectors
+            [MG, LG, FG] = quad.ConstructGlob(MElem, LElem, FElem)
+            
+            # Assemble Stiffness matrix and solving Ax = b
+            # Dirichlet B.Cs are solved using "Method of Large Number"
+            quad.Solve(MG, LG, FG)
+            
+            quad_err[i] = quad.l2
+            
+            # Plotting All Quad elements
+            plt.plot(quad.x, quad.u, '-', linewidth = '1', label = 'Grid Size: ' + str(quad.dx) )
+        
+        # Adding exact solution plot
+        plt.plot(quad.x, quad.u_ex, 'r--', linewidth = '1.6', label = "Exact Solution")
+        plt.title('Quadratic Elements')
+        plt.xlabel('x')
+        plt.ylabel('u')
+        plt.legend()
+        plt.grid()
+        
+        # Getting slope and y-intercept of loglog graph
+        m_lin, c_lin = np.polyfit(np.log(1/NN_el), np.log(lin_err), 1)   
+        m_quad, c_quad = np.polyfit(np.log(1/NN_el), np.log(quad_err), 1)
+        
+        # Plotting Error plots for both linear and quadratic
+        plt.figure()
+        plt.loglog(1/NN_el, lin_err, '-o', label = 'Linear')
+        plt.loglog(1/NN_el, quad_err, '-o', label = 'Quadratic')
+        plt.text(0.05, 0.0025, "Gradient: " + str("{:.2f}".format(m_lin)))
+        plt.text(0.05, 0.0000025, "Gradient: " + str("{:.2f}".format(m_quad)))
+        plt.title("L2-$norm$ vs Grid Size")
+        plt.legend()
+        plt.xlabel('Grid Size: log' + r'$\frac{1}{N_{el}}$ ', fontsize = 12)
+        plt.ylabel('log(L2-$norm$) ', fontsize = 12)
+        plt.grid()
+        plt.show()
+        
+
         
         
                 
--- main.py
+++ main.py
@@ -8,10 +8,25 @@ Created on Wed Apr 15 13:07:54 2020
 
 from class_helmholtz import Helmholtz
 
-test = Helmholtz(10,2)
-test.gauss_lobatto_laplacian()
-test.gauss_lobatto_mass()
-test.MatElem()
-test.MatGlob()
-test.SourceGlob()
-test.Solve()
+# Initialising Class with Helmholtz(N_el, p)
+# N_el -> No. of elements
+# P -> Polynomial order, p=1/2 for linear/quad
+case = Helmholtz(10, 1)
+
+# Initialising phi/dphi values at Gauss-Lobatto quadrature points
+case.init_gauss_lobatto()
+
+# Building Elemental Mass, Laplacian and Source matrices/vectors
+[MElem, LElem, FElem] = case.ConstructElem()
+
+# Constructing Global Mass, Laplacian and Source matrices/vectors
+[MG, LG, FG] = case.ConstructGlob(MElem, LElem, FElem)
+
+# Assemble Stiffness matrix and solving Ax = b
+# Dirichlet B.Cs are solved using "Method of Large Number"
+case.Solve(MG, LG, FG)
+
+# Plotting Solution Once
+case.PlotOnce()
+
+case.ErrorAnalysis()
